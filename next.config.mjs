a82960e3362204b596039ccb1dc7126ec99371c1
Jessie Odeh <jessieodeh08@gmail.com>
2024-02-14 18:37:20 +0100

    feat: improve load by unoptimizing compressed images

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,11 @@ import { join } from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  compress: true,
- async redirects() {
-  return [
-   // Basic redirect
-   {
-    source: '/server',
-    destination: '/',
-    permanent: true,
-   },
-  ];
- },
  sassOptions: {
   includePaths: [join(process.cwd(), 'src/styles')],
  },
  images: {
-  remotePatterns: [],
+  unoptimized: true,
  },
 };
 
