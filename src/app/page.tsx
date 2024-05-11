f5ddd36c9b2d27f38b7759ce11f917144711de30
Jessie Odeh <jessieodeh08@gmail.com>
2024-05-11 12:25:35 +0200

    - refactor ServerItem component mapping in data array
     This refactors the mapping of the ServerItem component in the data array by using the server metadata id as the key instead of the index. As in [type]/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default async function Home() {
  const data = (await getAll()).sort((a, b) => (b.data.players?.online || 0) - (a.data.players?.online || 0)).sort((a, b) => (b.data.online ? 1 : -1));
  const Servers = () => (
   <ServerListContainer>
-   {data.map((server, i) => {
-    return <ServerItem key={i} id={i} {...Object({ players: server.data.players, online: server.data.online })} metadata={server.metadata} />;
+   {data.map((server) => {
+    return <ServerItem key={server.metadata.id} id={server.metadata.id} {...Object({ players: server.data.players, online: server.data.online })} metadata={server.metadata} />;
    })}
   </ServerListContainer>
  );
